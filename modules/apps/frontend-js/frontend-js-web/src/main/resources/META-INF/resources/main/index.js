a264212ce12cc33abb763bc85db5f37f0e3ce98c
Gabriel Prates <gabsprates@gmail.com>
2026-02-27 08:57:09 +0100

    LPD-123123 Test CI

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/main/index.js
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/main/index.js
@@ -95,3 +95,7 @@ export {default as mimeTypeUtils} from './utils/mime_type/index';
 export {getObjectValueFromPath} from './utils/object/getObjectValueFromPath';
 export {default as pkceChallenge} from './utils/pkceChallenge';
 export {getFileAsBase64} from './utils/string/getFileAsBase64';
+
+export function testCI() {
+	return 1 + 2;
+}
