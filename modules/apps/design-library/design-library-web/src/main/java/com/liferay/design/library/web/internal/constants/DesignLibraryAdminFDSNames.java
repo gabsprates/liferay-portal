841bc53fd5ec911e30a935e0ac719eaf9afdcd12
Mario Leandro <mario.leandro@liferay.com>
2026-03-05 08:12:54 -0300

    LPD-80580 Add Design Library FDS constants

---
+++ modules/apps/design-library/design-library-web/src/main/java/com/liferay/design/library/web/internal/constants/DesignLibraryAdminFDSNames.java
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.design.library.web.internal.constants;
+
+/**
+ * @author Mario Leandro
+ */
+public class DesignLibraryAdminFDSNames {
+
+	public static final String ALL_DESIGN_LIBRARIES_SECTION =
+		DesignLibraryConstants.BUNDLE_SYMBOLIC_NAME +
+			"-allDesignLibrariesSection";
+
+}
