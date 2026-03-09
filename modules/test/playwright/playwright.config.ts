700b1f3034dec08c28acb8abf9863d8a40dcff93
Mario Leandro <mario.leandro@liferay.com>
2026-03-09 13:29:14 -0300

    LPD-80580 Add Playwright for Design Library Admin

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -62,6 +62,7 @@ import {config as contentDashboardWebConfig} from './tests/content-dashboard-web
 import {config as cookiesBannerWebConfig} from './tests/cookies-banner-web/main/config';
 import {config as dataCleanupConfig} from './tests/data-cleanup/main/config';
 import {config as depotWebConfig} from './tests/depot-web/main/config';
+import {config as designLibraryWebConfig} from './tests/design-library-web/main/config';
 import {config as digitalSalesRoomWebConfig} from './tests/digital-sales-room-web/main/config';
 import {config as dispatchWebConfig} from './tests/dispatch-web/main/config';
 import {config as documentLibraryWebConfig} from './tests/document-library-web/main/config';
@@ -261,6 +262,7 @@ export default defineConfig({
 		cookiesBannerWebConfig,
 		customerConfig,
 		depotWebConfig,
+		designLibraryWebConfig,
 		digitalSalesRoomWebConfig,
 		dispatchWebConfig,
 		documentLibraryWebConfig,
