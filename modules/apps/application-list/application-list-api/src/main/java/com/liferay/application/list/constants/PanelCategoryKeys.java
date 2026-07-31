bb6634b265ed6a27df47e494ac43c34fd80daa15
Gabriel Prates <gabsprates@gmail.com>
2026-07-31 18:30:43 +0200

    LPD-98002 Move Asset Libraries, Content Dashboard, and Publications to a new In Maintenance group

--- modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/constants/PanelCategoryKeys.java
+++ modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/constants/PanelCategoryKeys.java
@@ -35,6 +35,9 @@ public interface PanelCategoryKeys {
 	public static final String APPLICATIONS_MENU_APPLICATIONS_DESIGN =
 		"applications_menu.applications.design";
 
+	public static final String APPLICATIONS_MENU_APPLICATIONS_IN_MAINTENANCE =
+		"applications_menu.applications.in_maintenance";
+
 	public static final String APPLICATIONS_MENU_APPLICATIONS_PERSONALIZATION =
 		"applications_menu.applications.personalization";
 
