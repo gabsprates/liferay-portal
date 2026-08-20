6d42d3aa92f66664f00cfd77289da2896b579e72
Gabriel Prates <gabsprates@gmail.com>
2026-08-20 20:00:27 +0200

    LPD-98002 Rename Metrics to Workflow Metrics

--- modules/test/playwright/tests/object-web/entry/objectEntry.spec.ts
+++ modules/test/playwright/tests/object-web/entry/objectEntry.spec.ts
@@ -6728,7 +6728,7 @@ test.describe('Manage object entries through Workflow', () => {
 			applicationName
 		);
 
-		await globalMenuPage.goToApplications('Metrics');
+		await globalMenuPage.goToApplications('Workflow Metrics');
 
 		await metricsPage.chooseProcess(assetType);
 
